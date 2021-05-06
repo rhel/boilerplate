0df145c49ca23fb7e6b8d0446074dd78a83c69c2
Artyom Nosov <chip@unixstyle.ru>
2021-05-06 13:59:39 +0300

    the consul service has been added.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rhel/boilerplate
+
+go 1.14
+
+require github.com/hashicorp/consul/api v1.8.1
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	consulapi "github.com/hashicorp/consul/api"
+)
+
+type Service struct {
+	ID     string
+	Name   string
+	Consul *consulapi.Client
+}
+
+func (service *Service) Health(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("I'm fine. Thank you for asking!"))
+}
+
+func (service *Service) GetHandler(w http.ResponseWriter, r *http.Request) {
+	kv := service.Consul.KV()
+
+	key := r.URL.Query().Get("key")
+	pair, _, err := kv.Get(key, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if pair == nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("%v = nil", key))
+		http.Error(w, "", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintln(os.Stdout, fmt.Sprintf("%v = %s", pair.Key, pair.Value))
+	w.Write(pair.Value)
+}
+
+func (service *Service) SetHandler(w http.ResponseWriter, r *http.Request) {
+	kv := service.Consul.KV()
+
+	for key := range r.URL.Query() {
+		p := &consulapi.KVPair{
+			Key:   key,
+			Value: []byte(r.URL.Query().Get(key)),
+		}
+		_, err := kv.Put(p, nil)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	w.Write([]byte("Success"))
+}
+
+func GetMyIP() (net.IP, error) {
+	udp, err := net.Dial("udp", "8.8.8.8:53")
+	if err != nil {
+		return nil, err
+	}
+	defer udp.Close()
+
+	localAddr := udp.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP, nil
+}
+
+func main() {
+	ip, err := GetMyIP()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	c, err := consulapi.NewClient(consulapi.DefaultConfig())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	service := &Service{
+		ID:     "my-service",
+		Name:   "my-service",
+		Consul: c,
+	}
+
+	check := &consulapi.AgentServiceCheck{
+		Name:     "/health",
+		HTTP:     fmt.Sprintf("http://%s:%s/health", ip.String(), "8080"),
+		Interval: "10s",
+	}
+
+	registration := &consulapi.AgentServiceRegistration{
+		ID:      service.ID,
+		Name:    service.Name,
+		Address: ip.String(),
+		Port:    8080,
+		Check:   check,
+	}
+
+	err = service.Consul.Agent().ServiceRegister(registration)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	defer service.Consul.Agent().ServiceDeregister(service.ID)
+
+	http.HandleFunc("/health", service.Health)
+	http.HandleFunc("/get_value", service.GetHandler)
+	http.HandleFunc("/set_value", service.SetHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
